f6922f9b27fd550b314453c6bf40f741fff4f1b1
ekat-velaz <ekatvelaz@gmail.com>
2023-06-01 17:56:56 +0100

    Add bot AI and add game type choice

--- script.js
+++ script.js
@@ -7,7 +7,57 @@ const gameBoard = (() => {
     const gameBoardContainer = document.querySelector("#game-board");
     let boardArray = [null, null, null, null, null, null,null, null, null];
     let currentPlayer = 'playerX';
+    let bot;
+    let player;
+    let gameType;
+    const playerXBtn = document.querySelector('#player-X');
+    const playerOBtn = document.querySelector('#player-O');
+    const btnContainer = document.querySelector('#players-container');
+    btnContainer.style.visibility = 'hidden';
 
+    const playerChooseBtn = document.querySelector('#choose-player');
+    const botChooseBtn = document.querySelector('#choose-bot');
+    const chooseContainer = document.querySelector('#choose-container');
+
+    const handlePlayerChoice = (event) => {
+        chooseContainer.style.visibility = 'hidden';
+        createSquares();
+        gameType = 'players';
+    };
+
+    const handleBotChoice = (event) => {
+        chooseContainer.style.visibility = 'hidden';
+        btnContainer.style.visibility = 'visible';
+        gameType = 'bot';
+    };
+
+    playerChooseBtn.addEventListener('click', handlePlayerChoice);
+    botChooseBtn.addEventListener('click', handleBotChoice);
+    
+    const Player = (marker) => {
+        return {marker};
+    };
+
+    const handleXBtn = (event) => {
+        player = Player('X');
+        bot = Player('O');
+        createSquares();
+        playerOBtn.style.visibility='hidden';
+        playerXBtn.style.visibility='hidden';
+    };
+
+    const handleOBtn = (event) => {
+        player = Player('O');
+        bot = Player('X');
+        createSquares();
+        playerOBtn.style.visibility='hidden';
+        playerXBtn.style.visibility='hidden';
+        computerTurn();
+    };
+
+    playerOBtn.addEventListener('click', handleOBtn);
+    playerXBtn.addEventListener('click', handleXBtn);
+ 
     const createSquares = () => {
         boardArray.forEach((element, index) => {
             const square = document.createElement('button');
@@ -40,16 +90,30 @@ const gameBoard = (() => {
     const handleEvent = (event) => {
         const button = event.target;
         const indexValue = button.getAttribute('id');
-        if (currentPlayer === "playerX" && boardArray[indexValue] === null) {
-            boardArray[indexValue] = 'X';
-            button.textContent = ('X');
-            togglePlayer();
-            checkWinner();
-        } else if (currentPlayer === "playerO" && boardArray[indexValue] === null) {
-            boardArray[indexValue] = 'O';
-            button.textContent = ('O');
-            togglePlayer();
-            checkWinner();
+        if (gameType === 'bot') {
+            if (player.marker === 'X' && boardArray[indexValue] === null) {
+                boardArray[indexValue] = 'X';
+                button.textContent = ('X');
+                checkWinner();
+                computerTurn();
+            } else if (player.marker === 'O' && boardArray[indexValue] === null) {
+                boardArray[indexValue] = 'O';
+                button.textContent = ('O');
+                checkWinner();
+                computerTurn();
+            };
+        } else if (gameType === 'players') {
+            if (currentPlayer === "playerX" && boardArray[indexValue] === null) {
+                boardArray[indexValue] = 'X';
+                button.textContent = ('X');
+                togglePlayer();
+                checkWinner();
+            } else if (currentPlayer === "playerO" && boardArray[indexValue] === null) {
+                boardArray[indexValue] = 'O';
+                button.textContent = ('O');
+                togglePlayer();
+                checkWinner();
+            };
         };
     };
 
@@ -83,17 +147,23 @@ const gameBoard = (() => {
         createSquares();
     };
 
+    const computerTurn = () => {
+    cells = [];
+    for (i = 0; i < 10; i++) {
+      if (boardArray[i] === null) cells.push(i);
+    }
+    const randomNumber = Math.floor(Math.random() * cells.length);
+    const squareToFill = document.getElementById(`${cells[randomNumber]}`)
+    if (bot.marker === 'X') {
+        squareToFill.textContent = ('X');
+    } else if (bot.marker === 'O') {
+        squareToFill.textContent = ('O');
+    };
+    };
+
     return {
         createSquares,
+        computerTurn,
     };
 
 })();
-
-gameBoard.createSquares();
-
-const Player = (marker) => {
-    this.marker = marker;
-    const playerXBtn = document.querySelector('#player-X');
-    const playerOBtn = document.querySelector('#player-O');
-}
-
